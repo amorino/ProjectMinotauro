c3b2b039123473068b090fbfa47bf0db13381d02
Sergio Martínez <sergio.mtz.campos@gmail.com>
2016-09-07 00:20:51 -0500

    Indicadores para Plataformas

---
+++ Assets/Scripts/IndicatorPositions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class IndicatorPositions : MonoBehaviour {
+    public enum EntryType
+    {
+        Vertical,
+        Horizontal,
+        None
+    };
+
+    public List<GameObject> indicator;
+    public List<EntryType> entry;
+
+    void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
