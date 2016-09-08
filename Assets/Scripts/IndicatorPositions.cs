25aab6dfe8db4b6f96d27e9be3cd5546b12da2b5
Sergio Martínez <sergio.mtz.campos@gmail.com>
2016-09-07 23:57:27 -0500

    Posicion y Angulo de indicadores.

--- Assets/Scripts/IndicatorPositions.cs
+++ Assets/Scripts/IndicatorPositions.cs
@@ -3,6 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class IndicatorPositions : MonoBehaviour {
+
+    public List<GameObject> indicator;
+    public List<EntryType> entry;
+
     public enum EntryType
     {
         Vertical,
@@ -10,14 +14,41 @@ public class IndicatorPositions : MonoBehaviour {
         None
     };
 
-    public List<GameObject> indicator;
-    public List<EntryType> entry;
+    void Start ()
+    {
+        SetIndicators();
+	}
+	
+    void SetIndicators()
+    {
+        for (int i = 0, iEnd = indicator.Count; i < iEnd; i++)
+        {
+            SetPosition(indicator[i].transform, i);
+            SetAngle(indicator[i].transform, entry[i], i);
+        }
+    }
 
-    void Start () {
-	
-	}
-	
-	void Update () {
-	
-	}
+    void SetAngle(Transform indicator, EntryType entry, int index)
+    {
+        float eulerY = 0;
+        if (entry == EntryType.Horizontal)
+            eulerY = (index == 0 || index == 3) ? 0 : 180;
+
+        if (entry == EntryType.Vertical)
+            eulerY = (index == 0 || index == 1) ? 90 : 270;
+
+        if (entry == EntryType.None)
+            eulerY = 90 * index;
+
+        indicator.localRotation = Quaternion.Euler(new Vector3(indicator.eulerAngles.x, eulerY, indicator.eulerAngles.z));
+    }
+
+    void SetPosition(Transform indicator, int index)
+    {
+        float posX = (index == 0 || index == 3) ? -0.25f : 0.25f;
+        float posY = (index == 2 || index == 3) ? -0.25f : 0.25f;
+
+        indicator.localPosition = new Vector3(posX, indicator.localPosition.y, posY);
+    }
+    
 }
