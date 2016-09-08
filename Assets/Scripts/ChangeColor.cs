732a0246fedbebfab46a830da415a7e2e19ef8ed
Erick Bazán <bazan.erick@gmail.com>
2016-09-08 02:18:13 -0500

    Change Color Script

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColor : MonoBehaviour {
+
+	bool drag = false;
+	Material originalMaterial;
+	Renderer rend;
+	GameObject cube;
+	Color color;
+
+	// Use this for initialization
+	void Start () {
+		cube = transform.Find("Cube").gameObject;
+		rend = cube.GetComponent<Renderer>();
+		rend.enabled = true;
+		originalMaterial = rend.sharedMaterial;
+	}
+
+	void Update()
+	{
+		color = rend.material.color;
+		color.b = 255.0f;
+		if (drag) {
+			rend.sharedMaterial.color = color;
+		} else {
+			rend.sharedMaterial = originalMaterial;
+		}
+	}
+
+	public void setDrag(){
+		drag = true;
+	}
+
+	public void StopDrag()
+	{
+		drag = false;
+	}
+}
